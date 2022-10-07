e87eb84482e85b0613b078e39484a7169b599341
ccg <ccgjava@163.com>
2022-10-07 18:46:18 +0800

    在Person类中添加属性

--- TestModule01/src/com/ccg/Person.java
+++ TestModule01/src/com/ccg/Person.java
@@ -1,8 +1,11 @@
 package com.ccg;
 
+import java.math.BigDecimal;
+
 public class Person {
     private String name ;// 
     private int age ; //
-    
+    private BigDecimal height; //
+
     
 }
